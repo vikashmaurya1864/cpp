567a7c742abde6bd7ccef50e6068ef119cf2ee2d
vikash singh maurya <vikashmaurya1864@gmail,com>
2021-10-07 15:40:16 +0530

    if else file

---
+++ if_else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+     int saving;
+     cout<<"enter you saving man"<<endl;
+     cin>>saving;
+
+     if(saving>5000){
+         cout<<"neha is better choice \n";
+
+     }   else {
+         cout<<"rasmika is better choice\n";
+     }
+
+     return 0;
+
+ }
