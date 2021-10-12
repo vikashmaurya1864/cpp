f36c189eb987dcbb3254c5347c9224c18998d556
vikash singh maurya <vikashmaurya1864@gmail,com>
2021-10-12 21:01:10 +0530

    first time swithimplemation

---
+++ switchimplemation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"enter a age,i give correct suggestion"<<endl;
+    cin>>age;
+    switch(age)
+    {
+        case 17:
+        cout<<"you are good person if you age is 17"<<endl;
+        break;
+    
+        case 18:
+        cout<<"you are just 18 go outside explore thing "<<endl;
+        break;
+    
+        case 22:
+        cout<<"focus you coding skill"<<endl;
+        break;
+
+        default:
+        cout<<"focus you coding skill"<<endl;
+        break;
+    
+    }
+
+}   
