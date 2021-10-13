bd33c9edb81d806be13a8e8c7d80dd18115cd59a
vikash singh maurya <vikashmaurya1864@gmail,com>
2021-10-13 10:47:07 +0530

    fuctionprogram

---
+++ fuction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void sum()
+{
+    int a,b,c;
+    cout<<"enter the two number"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is "<<c<<endl;
+}
+
+int main()
+{
+    void sum(void);
+    sum();
+}
