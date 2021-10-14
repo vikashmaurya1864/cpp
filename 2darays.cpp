fa2ab340d377b3c08455eb2c8efb47bac3ebbc3d
vikash singh maurya <vikashmaurya1864@gmail,com>
2021-10-14 20:20:38 +0530

    2d

---
+++ 2darays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[3][2]={{10,15},{13,41},{21,13}};
+    cout<<a[0] [0]<<endl;
+    cout<<a[1] [0]<<endl;
+    cout<<a[2] [0]<<endl;
+    cout<<a[0] [1]<<endl;
+    cout<<a[1] [1]<<endl;
+    cout<<a[2] [1]<<endl;
+}
