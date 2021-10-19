48d0affcfb63214ea18e236f3a490421439bbd40
vikash singh maurya <vikashmaurya1864@gmail,com>
2021-10-19 09:20:35 +0530

    array implemention

---
+++ .vscode/array.cpp
@@ -0,0 +1,18 @@
+//we are going to declare arry two type 
+//one defalut and another one is declare arry user defind
+#include <iostream>
+using namespace std;
+int main(){
+    string arr[5];
+
+    int n;
+    string arr1[n];//user defind array
+
+    int arr2[5]={1,2,3,4,5}; 
+    //thesse are three type declare array here how to acces 
+    //them let's   go to take overview;
+
+    cout<<arr2[3]<<endl;
+    arr2[3]=134;// we are access the value and change it
+    cout<<arr2[3]<<endl;
+}
